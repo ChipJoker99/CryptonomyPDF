9a96cdbc4c4a127f126fc33680a4ce27009b5ea8
ChipJoker99 <82726784+ChipJoker99@users.noreply.github.com>
2024-07-23 10:23:36 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,15 +1,40 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 from PyPDF2 import PdfFileReader, PdfFileWriter
+import os
 
-def encrypt(input_file, password):
-    pdf = PdfFileReader(input_file)
-    writer = PdfFileFileWriter()
-    for n in range (pdf.getNumPages()):
-        page = pdf.getPage(n)
-        writer.addPage(page)
+def encrypt_pdf(inputPdf, outputPdf, key):
+    pdfWriter = pdfFileWriter()
+    pdfReader = pdfFileReader(inputPdf)
+    
+    for pageNum in range (pdfReader.getNumPages()):
+        pdfWriter.addPage(pdfReader.getPage(pageNum))
 
-    writer.encrypt(password)
+    pdfWriter.encrypt(userPwd=key, ownerPwd=None, use128bit=True)
 
-    with open('encrypted_' + input_file, 'wb') as output_file:
-        writer.write(output_file)
+    with open(outputPdf, 'wb') as f:
+        pdfWriter.write(f)
 
-encrypt('file.pdf', 'password')
+def generateKey():
+    return getRandomBytes(16)
+
+def saveKey(key, filename):
+    with open(filename, 'wb') as f:
+        f.write(key)
+
+def loadKey(filename):
+    with open(filename, 'rb') as f:
+        return f.read()
+
+if __name__ == '__main__':
+    inputPdf = 'input.pdf'
+    outputPdf = 'encrypted_output.pdf'
+    keyFile = 'encryption_key.key'
+
+    key = generate_key()
+    save_key(key, keyFile)
+
+    encrypt_pdf(inputPdf, outputPdf, key)
+
+    print(f'PDF encrypted and saved as {outputPdf}')
+    print(f'Encryption key saved as {keyFile}')
